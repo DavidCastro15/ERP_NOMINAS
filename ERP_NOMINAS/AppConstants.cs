47615b97f762308474b3e8610970abd0ab9e0437
DavidCastro15 <20183L301044@utcv.edu.mx>
2026-04-09 14:39:28 -0600

    feat(config) : Add IP and password reading from App.config

--- ERP_NOMINAS/AppConstants.cs
+++ ERP_NOMINAS/AppConstants.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,8 @@ namespace ERP_NOMINAS
     {
         public const string AppName = "ERP_NOMINAS";
         public const string CompanyName = "AZUCARERA SAN JOSE DE ABAJO, S.A DE C.V";
+        public const string ServerIp = "192.168.2.250";
+        public const string User = "sa";
+        public static readonly string Password = ConfigurationManager.AppSettings["DB_PASSWORD"]; //MANERA MAS RAPIDA MENOS SEGURA !!USAR AppConfig para version final
     }
 }
--- ERP_NOMINAS/Conexion/ConnectorSql.cs
+++ ERP_NOMINAS/Conexion/ConnectorSql.cs
@@ -6,17 +6,14 @@ namespace ERP_NOMINAS.Conexion
     public class ConnectorSql
     {
         private string separators = "$";
-        private string commands = Environment.CommandLine; // equivalente a Command() en VB
+        private string commands = Environment.CommandLine; 
         private string[] args;
-
-        private string ip_servidor = "192.168.2.250";
+ 
         //private string base_datos_2 = "erp_nomina";
         // private string ip_servidor = "187.157.242.105";
         // private string base_datos = "azsja_nomina_pruebas"; // PRUEBAS
-        private string user = "sa";
-        private string pwd = "AsjaEvol19";
 
-        private string base_datos;
+        private string DB;
 
         public SqlConnection asis;
         public SqlConnection nomi;
@@ -25,13 +22,13 @@ namespace ERP_NOMINAS.Conexion
         public ConnectorSql()
         {
             args = commands.Split(separators.ToCharArray());
-            base_datos = args.Length > 1 ? args[1].ToString() : "azsja_nomina";
+            DB = args.Length > 1 ? args[1].ToString() : "azsja_nomina";
 
             asis = new SqlConnection(
-                $"Data Source={ip_servidor};Initial Catalog=erpnomina;Persist Security Info=True;User ID={user};Password={pwd};MultipleActiveResultSets=True");
+                $"Data Source={AppConstants.ServerIp};Initial Catalog=erpnomina;Persist Security Info=True;User ID={AppConstants.User};Password={AppConstants.Password};MultipleActiveResultSets=True");
 
             nomi = new SqlConnection(
-                $"Data Source={ip_servidor};Initial Catalog={base_datos};Persist Security Info=True;User ID={user};Password={pwd};MultipleActiveResultSets=True;Max Pool Size=10024;Connection Timeout=999");
+                $"Data Source={AppConstants.ServerIp};Initial Catalog={DB};Persist Security Info=True;User ID={AppConstants.User};Password={AppConstants.Password};MultipleActiveResultSets=True;Max Pool Size=10024;Connection Timeout=999");
 
             //conexion3 = new SqlConnection(
             //    $"Data Source={ip_servidor};Initial Catalog={base_datos_2};Persist Security Info=True;User ID={user};Password={pwd};MultipleActiveResultSets=True;Max Pool Size=10024;Connection Timeout=999");
